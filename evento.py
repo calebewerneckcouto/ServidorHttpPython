16e9f9cb1a5a3081ca9954117a71ded07309d215
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-25 16:21:15 -0300

    heranca python

--- evento.py
+++ evento.py
@@ -13,10 +13,7 @@ class Evento:
         print(f"Local do evento:",{self.local})
         print("------------------------------")
         
-    @classmethod
-    def cria_evento_online(cls,nome):
-        evento = cls(nome,local=f"https://tamarcado.com/eventos?id={cls.id}") 
-        return evento   
+    
     
     @staticmethod
     def calcula_limite_pessoas_area(area):
@@ -29,11 +26,26 @@ class Evento:
         else:
             return 0
         
+        
+class EventoOnline(Evento):
+    def __init__(self, nome, _=""):
+        local = f"https://tamarcado.com/eventos?id={EventoOnline.id}"
+        super().__init__(nome,local)
+    
+    def imprime_informacoes(self):
+        print(f"ID do evento",{self.id})
+        print(f"Nome do evento:",{self.nome})    
+        print(f"Link para acessar o evento:",{self.local})
+        print("------------------------------")        
+    
+         
 ev = Evento("Aula de Python")
 ev2 = Evento("Aula de Javascript","Rio de Janeiro")
 
-ev_online = Evento.cria_evento_online("Live de Python")
-ev2_online = Evento.cria_evento_online("Live de Javascript")
+ev_online = EventoOnline("Live de Python")
+ev2_online = EventoOnline("Live de Javascript")
 ev_online.imprime_informacoes()   
 ev2_online.imprime_informacoes()
 # print(Evento.calcula_limite_pessoas_area(5))  
+ev =Evento ("Aula de Python","Rio de Janeiro")
+ev.imprime_informacoes()
