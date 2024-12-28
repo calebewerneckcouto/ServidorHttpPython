a276325f4475691f88ce65d89759baca5d4bab24
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-28 00:16:47 -0300

    Deletar evento

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from evento import Evento
 from evento_online import EventoOnline
-from flask import Flask, jsonify,make_response,abort
+from flask import Flask, jsonify,make_response,abort,request,json
 
 app = Flask(__name__)
 
@@ -33,7 +33,20 @@ def nao_encontrado(erro):
     return (jsonify(data),404)
 
 
-@app.route("/api/eventos/<int:id>")
+@app.errorhandler(400)
+def nao_encontrado(erro):
+    data={"erro":str(erro)}
+    return (jsonify(data),400)
+
+
+def get_evento_or_404(id):
+    for ev in eventos:
+        if ev.id == id:
+            return ev
+    
+
+
+@app.route("/api/eventos/<int:id>/")
 def buscar_id(id):
     """
     Busca um evento específico pelo seu ID.
@@ -42,11 +55,55 @@ def buscar_id(id):
     :type event_id: int
     :return: JSON com os detalhes do evento se encontrado, ou uma mensagem de erro.
     """
-    event = next((ev for ev in eventos if ev.id == id), None)
-    if event:
-        return jsonify(event.to_dict())
+    ev = get_evento_or_404(id)
+    if ev:
+        return jsonify(ev.to_dict())
     data={"erro":f"Não encontrei o evento com id: {id}"}
     abort(404,"Evento não encontrado")
+    
+    
+@app.route("/api/eventos/<int:id>/", methods=["DELETE"])
+def deletar_evento(id):
+    """
+    Deletar um evento específico pelo seu ID.
+    
+    :param id: ID do evento a ser buscado.
+    :type id: int
+    :return: JSON com os detalhes do evento se encontrado, ou uma mensagem de erro.
+    """
+    ev = get_evento_or_404(id)
+    if ev:
+        eventos.remove(ev)
+        return jsonify(id=id)
+    
+    abort(404, "Evento não encontrado")
+
+       
+    
+    
+@app.route("/api/eventos/", methods=["POST"])  
+def criar_eventos():  
+    data = json.loads(request.data)
+    nome = data.get("nome")
+    local = data.get("local")
+    
+    #Validação
+    if not nome:
+         abort(400," 'Nome'  deve ser informado")
+    elif not local:
+        abort(400," 'local' deve ser informado")     
+    
+    if local:
+        evento = Evento(nome=nome,local=local)
+    else:
+        evento = EventoOnline(nome=nome)
+    
+    
+    eventos.append(evento)
+    return {
+        "id": evento.id,
+        "url":f"/api/eventos/{evento.id}/"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
